c4a4cd77c6776154e8c9b37b6df642b382ab0356
valter emilio <valternic77@gmail.com>
2025-05-06 21:30:22 -0300

    primeiro commit

---
+++ src/br/com/valter/ConvesorDeMoedas/Modelos/ConvesorDeMoedas.java
@@ -0,0 +1,4 @@
+package br.com.valter.ConvesorDeMoedas.Modelos;
+
+public class ConvesorDeMoedas {
+}
---
+++ src/br/com/valter/ConvesorDeMoedas/Modelos/Menu.java
@@ -0,0 +1,19 @@
+package br.com.valter.ConvesorDeMoedas.Modelos;
+
+public class Menu {
+    public void mostrarMenu(){
+        System.out.println("""
+                =================================================================
+                1 - Dolar(USD) =>> Peso Argentino(ARS);
+                2 - Peso Argentino(ARS) =>> Dolar(USD);
+                3 - Boliviano da Bolivia(BOB) =>> Real Brasileiro(BRL);
+                4 - Real Brasileiro(BRL) =>> Boliviano da Bolivia(BOB);
+                5 - Peso Chileno(CLP) =>> Peso Colombiano(COP);
+                6 - Peso Colombiano(COP) =>> Peso Chileno(CLP);
+                7 - Sair(|);
+                8 - Consultar Histórico de Convesões;
+                ------------------------------------------------------------------
+                Escolha a opção para qual moeda Deseja Converter:
+                """);
+    }
+}
